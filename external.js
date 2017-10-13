e75c80395ff460c05c0870ab36f58797ead20a68
Tony Westbrook <tonywestbrook58@mail.com>
2017-10-13 09:14:22 -0500

    added codeup design project with fucntion exercise

---
+++ external.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website");
+var yourcolor = prompt("what color do you like?");
+alert("Great " + yourcolor + " is my favorite color too!");

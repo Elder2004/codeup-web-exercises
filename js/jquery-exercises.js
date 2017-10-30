310efc3e5b2f92ab8746fa23d3398e965c0a5aac
Tony Westbrook <tonywestbrook58@mail.com>
2017-10-30 16:25:05 -0500

    jquery lessons updated

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,9 +1,25 @@
 "use strict";
 
  $(document).ready(function() {
-    var contents =$("#paragraph").html();
-   alert( 'The DOM has finished loading!' );
-   $('h1').css('font size', '20px')
+   $('#ListofFriends').click(
+       function() {
+           $('h1').css('background-color', 'red');
+       }
+
+   );
 });
 
+$('div').dbclick(function() {
+    $(this).css('font-size', '18px)';
+    alert('paragraph  was double clicked!');
+});
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
 
